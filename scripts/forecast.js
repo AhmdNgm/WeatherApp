8be347c412171a9272e69e1050f5b8f0a46b56ad
AhmdNgm <ahmdn4418@gmail.com>
2022-04-19 06:39:11 +0200

    Trigger rebuild

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
   constructor() {
     this.key = 'V7SQ5t4QbTwpz8HXfE2C1EmvAybS9afJ';
-    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   }
   async updateCity(city) {
     const cityDets = await this.getCity(city);
